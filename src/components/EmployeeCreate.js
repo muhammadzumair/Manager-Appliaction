777f2bb2ba1a25f104ed1f2373af9123d77fe568
MuhammadZumair <zumairmuhammad@gmail.com>
2017-05-13 11:20:15 +0500

    modified the employeeCreate

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -11,14 +11,7 @@ class EmployeeCreate extends Component{
     }
     render(){
         return(
-            <Card>
-               <EmployeeForm {...this.props} />
-                <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Create
-                    </Button>
-                </CardSection>
-            </Card>
+            
         );
     }
 }
